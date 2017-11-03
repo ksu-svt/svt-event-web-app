9e4a2dfdd660953bbb3f686fa993a1436452a2c7
Thomas Fuller <mtfuller1995@gmail.com>
2017-11-03 00:51:11 -0400

    Added initial event models

--- event_manager/models.py
+++ event_manager/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    title = models.CharField(max_length=100)
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    team = models.OneToOneField(Team, null=True, on_delete=models.SET_NULL)
+    location = models.CharField(max_length=100)
+    datetime = models.DateTimeField()
+    description = models.CharField(max_length=256)
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+    role = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    points = models.IntegerField()
+    is_active = models.BooleanField()
+    has_paid_dues = models.BooleanField()
+    team = models.OneToOneField(Team, null=True, on_delete=models.SET_NULL)
+    events = models.ManyToManyField(Event)
