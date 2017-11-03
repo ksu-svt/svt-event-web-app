e8fa7c8139c13bf40152f17aa6b20808280ba070
Thomas Fuller <mtfuller1995@gmail.com>
2017-11-03 01:12:41 -0400

    Fixed issue with many to many field being required.

--- event_manager/models.py
+++ event_manager/models.py
@@ -3,12 +3,8 @@ from django.db import models
 class Team(models.Model):
     title = models.CharField(max_length=100)
 
-class Event(models.Model):
-    title = models.CharField(max_length=100)
-    team = models.OneToOneField(Team, null=True, on_delete=models.SET_NULL)
-    location = models.CharField(max_length=100)
-    datetime = models.DateTimeField()
-    description = models.CharField(max_length=256)
+    def __str__(self):
+        return self.title+" Team"
 
 class Member(models.Model):
     firstname = models.CharField(max_length=50)
@@ -20,4 +16,20 @@ class Member(models.Model):
     is_active = models.BooleanField()
     has_paid_dues = models.BooleanField()
     team = models.OneToOneField(Team, null=True, on_delete=models.SET_NULL)
-    events = models.ManyToManyField(Event)
+
+    def full_name(self):
+        return "{} {}".format(self.firstname, self.lastname)
+
+    def __str__(self):
+        return self.full_name()
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    team = models.OneToOneField(Team, null=True, on_delete=models.SET_NULL)
+    location = models.CharField(max_length=100)
+    datetime = models.DateTimeField()
+    description = models.CharField(max_length=256)
+    members = models.ManyToManyField(Member, blank=True)
+
+    def __str__(self):
+        return self.title
