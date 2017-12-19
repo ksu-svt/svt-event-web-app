d2b8bb30c92ed159d94d656ce9d4c0e4700634f0
Karim Rattani <karim_rattani@hotmail.com>
2017-12-18 20:49:19 -0500

    added models

--- event_manager/models.py
+++ event_manager/models.py
@@ -1,5 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class member(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    last_name=models.CharField(max_length=200)
+    netid=models.IntegerField(default=0)
+    role=models.CharField(max_length=200)
+    major=models.CharField(max_length=200)
+    points=models.IntegerField(default=0)
+
+class teams(models.Model):
+    user=models.ForeignKey(member,on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+
+class events(models.Model):
+    team=models.ForeignKey(teams,on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    location=models.CharField(max_length=200)
+    dateTime=models.DateTimeField()
+    description=models.CharField(max_length=200)
+    user=models.ManyToManyField(member)
 
 
 # Create your models here.
