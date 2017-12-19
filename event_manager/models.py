b63cbd9556eb20bf4c09ec756fe354fe44c54d4f
Thomas Fuller <mtfuller1995@gmail.com>
2017-12-18 22:38:52 -0500

    Applied model changes. Fixed "int 10" error.

--- event_manager/models.py
+++ event_manager/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class member(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    last_name=models.CharField(max_length=200)
-    netid=models.IntegerField(default=0)
-    role=models.CharField(max_length=200)
-    major=models.CharField(max_length=200)
-    points=models.IntegerField(default=0)
+class Member(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    netid = models.IntegerField(default=0)
+    role = models.CharField(max_length=200)
+    major = models.CharField(max_length=200)
+    points = models.IntegerField(default=0)
 
-class teams(models.Model):
-    user=models.ForeignKey(member,on_delete=models.CASCADE)
-    title=models.CharField(max_length=200)
+class Team(models.Model):
+    title = models.CharField(max_length=200)
+    lead = models.ForeignKey(Member, null=True, on_delete=models.SET_NULL)
+    members = models.ManyToManyField(Member, related_name="team_members")
 
-class events(models.Model):
-    team=models.ForeignKey(teams,on_delete=models.CASCADE)
-    title=models.CharField(max_length=200)
-    location=models.CharField(max_length=200)
-    dateTime=models.DateTimeField()
-    description=models.CharField(max_length=200)
-    user=models.ManyToManyField(member)
+class Event(models.Model):
+    team = models.ForeignKey(Team,on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    dateTime = models.DateTimeField()
+    description = models.CharField(max_length=200)
+    user = models.ManyToManyField(Member)
 
 
 # Create your models here.
