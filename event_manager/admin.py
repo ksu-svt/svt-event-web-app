5f31a8bb081c5f906314d44fc527e1ecb7b755dd
Karim Rattani <karim_rattani@hotmail.com>
2017-12-19 13:26:57 -0500

    added member in user registration form

--- event_manager/admin.py
+++ event_manager/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Team, Event, Member
+from .models import Team, Event, UserAdmin
+from django.contrib.auth.models import User
 
 admin.site.register(Team)
 admin.site.register(Event)
-admin.site.register(Member)
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
--- event_manager/models.py
+++ event_manager/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.http import HttpRequest
 
 class Member(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -8,10 +12,16 @@ class Member(models.Model):
     major = models.CharField(max_length=200)
     points = models.IntegerField(default=0)
 
+    def __str__(self):
+        return User.first_name+" "+User.last_name
+
 class Team(models.Model):
     title = models.CharField(max_length=200)
     lead = models.ForeignKey(Member, null=True, on_delete=models.SET_NULL)
     members = models.ManyToManyField(Member, related_name="team_members")
+
+    def __str__(self):
+        return Team.title
 
 class Event(models.Model):
     team = models.ForeignKey(Team,on_delete=models.CASCADE)
@@ -20,6 +30,22 @@ class Event(models.Model):
     dateTime = models.DateTimeField()
     description = models.CharField(max_length=200)
     user = models.ManyToManyField(Member)
+
+    def __str__(self):
+        return Event.title
+
+class MemberInline(admin.StackedInline):
+    model=Member
+    max_num=1
+
+class UserAdmin(AuthUserAdmin):
+    inlines=(MemberInline,)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(UserAdmin, self).get_inline_instances(request, obj)
+
 
 
 # Create your models here.
