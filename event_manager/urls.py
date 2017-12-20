8338b5347c991ae7e5390b42573a0e2c6479294f
VASUKESH YARLAGADDA <yarlagad@gmail.com>
2017-12-20 17:52:38 -0500

    Added signup and login form

--- event_manager/urls.py
+++ event_manager/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 from . import views
-# Login Function within Django Framework
-from django.contrib.auth.views import login
+
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^login/$', login, {'template_name': 'event_manager/login.html'})
+    url(r'^login/$', views.login_form, {'template_name': 'event_manager/login.html'})
 ]
--- event_manager/views.py
+++ event_manager/views.py
@@ -1,4 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# Signup Authentication
+from django.contrib.auth.forms imports UserCreationForm, AuthenticationForm
+# Login Function within Django Framework
+from django.contrib.auth.views import login
+
 
 def home(request):
     return render(request, 'event_manager/home.html')
+
+
+def signup_form(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # log the user in
+            return redirect('/home/')  # trying to go to login page
+    else:
+        form = UserCreationForm()
+        # Please Add Sign Up form View Here (render, View, 'form':form)
+    return render(request, 'event_manager/signup.html', {'form': form})
+    pass
+
+
+def login_form(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/home/')  # using Login_redirect_url on settings
+    else:
+        form = AuthenticationForm()
+    return render(request, 'event_manager/login.html', {'form': form})
