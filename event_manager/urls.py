168f1381085efec8a5db108e6730144c463365cf
VASUKESH YARLAGADDA <yarlagad@gmail.com>
2017-12-20 17:59:56 -0500

    small addition to urls.py, still need signup template

--- event_manager/urls.py
+++ event_manager/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', views.login_form, {'template_name': 'event_manager/login.html'})
+    url(r'^signup/$', views.signup_form, {'template_name': 'event_manager/signup.html'})
 ]
