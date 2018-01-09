196eff75927683335ef576819749d650ff6ca329
Thomas Fuller <mtfuller1995@gmail.com>
2018-01-09 13:50:39 -0500

    Removed unnecessary "requests" module.

--- event_manager/forms.py
+++ event_manager/forms.py
@@ -2,7 +2,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from .models import Member
-import requests
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(required=True)
